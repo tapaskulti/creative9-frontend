abdd3632f9a195d30eea8e45cba2e39b5e3a2a67
Tapas Chandra <tapaskulti@gmail.com>
2025-07-31 02:59:11 +0530

    freelancer page

--- src/components/Freelancer.jsx
+++ src/components/Freelancer.jsx
@@ -1,7 +1,8 @@
 import { Link } from "react-router-dom";
 import Footer from "./Footer";
 import Header from "./Header";
-import FiverrImage from "../assets/fiver-page.jpg";
+import FiverrImage from "../assets/fiverr-profile.jpg";
+import FiverrLogo from "../assets/fiverr.png";
 
 export default function Freelancer() {
   return (
@@ -9,80 +10,166 @@ export default function Freelancer() {
       <Header />
       <div className="py-20">
         <div className="px-10 3xl:px-64 flex gap-5">
-          <div className="w-full h-96 md:w-80 lg:w-72 xl:w-80 2xl:w-80 3xl:w-96 shadow-lg  rounded-lg hover:shadow-xl border border-slate-200 relative cursor-pointer">
-            <div className="flex justify-center text-center">
-              {/* <img src={image} className="h-full rounded-lg w-72 sm:w-80 md:w-96 3xl:w-80" /> */}
+          <Link to="https://www.fiverr.com/creativevalley9">
+            <div className="w-full h-[690px] md:w-80 lg:w-72 xl:w-96 2xl:w-[420px] 3xl:w-[480px] shadow-lg rounded-lg hover:shadow-xl border border-slate-200 relative cursor-pointer">
+              <div className="text-center">
+                {/* <img src={image} className="h-full rounded-lg w-72 sm:w-80 md:w-96 3xl:w-96" /> */}
+                <img
+                  src={FiverrLogo}
+                  alt=""
+                  className="w-auto h-full rounded-xl"
+                />
+                <img
+                  src={FiverrImage}
+                  className="rounded-t-lg object-cover w-full h-auto"
+                />
+              </div>
+              <div className="px-4 py-3">
+                <h3 className="font-bold">About me</h3>
+                <h2 className="text-base font-medium py-2">
+                  BEST QUALITY ON TIME AT FARE PRICE
+                </h2>
+                <p className="text-sm leading-[22px]">
+                  hi there, I'm prabir Sarkar. I've done 1100 plus projects with
+                  850+ great reviews. I've completed 4 years degree course on
+                  painting from the Indian college of arts and draftsmanship
+                  from kolkata in India. From 2009 till now i'm working as a
+                  freelance illustrator and painter. I'm Working on children
+                  Book illustration, African American children book
+                  illustration, character illustration, 2d Game Design, Logo
+                  Design, Conceptual Drawing, Background illustration,portrait
+                  illustration, caricature illustration, t-shirt Design.
+                  <h2 className="py-2">Hit me on "creativevalley9".</h2>
+                </p>
+                <h2 className="text-sm font-semibold">
+                  Thanks,
+                  <br />
+                  creativevalley9
+                </h2>
+              </div>
+              <div className="bg-[#1dbf73] text-white font-semibold text-center text-lg py-3 absolute bottom-0 w-full rounded-b-lg">
+                Visit my Fiver profile
+              </div>
+            </div>
+          </Link>
+          <div className="w-full h-[690px] md:w-80 lg:w-72 xl:w-96 2xl:w-[420px] 3xl:w-[480px] shadow-lg rounded-lg hover:shadow-xl border border-slate-200 relative cursor-pointer">
+            <div className="text-center">
+              {/* <img src={image} className="h-full rounded-lg w-72 sm:w-80 md:w-96 3xl:w-96" /> */}
+              <img
+                src={FiverrLogo}
+                alt=""
+                className="w-auto h-full rounded-xl"
+              />
               <img
                 src={FiverrImage}
-                className="rounded-t-lg object-cover w-full h-24"
+                className="rounded-t-lg object-cover w-full h-auto"
               />
             </div>
-            <div className="px-4 py-3 text-[15px]">
-              hi there, I'm prabir Sarkar. I've done 1100 plus projects with
-              850+ great reviews. I've completed 4 years degree course on
-              painting from the Indian college of arts and draftsmanship from
-              kolkata in India. From 2009 till now i'm working as a freelance
-              illustrator and painter.
+            <div className="px-4 py-3">
+              <h3 className="font-bold">About me</h3>
+              <h2 className="text-base font-medium py-2">
+                BEST QUALITY ON TIME AT FARE PRICE
+              </h2>
+              <p className="text-sm leading-[22px]">
+                hi there, I'm prabir Sarkar. I've done 1100 plus projects with
+                850+ great reviews. I've completed 4 years degree course on
+                painting from the Indian college of arts and draftsmanship from
+                kolkata in India. From 2009 till now i'm working as a freelance
+                illustrator and painter. I'm Working on children Book
+                illustration, African American children book illustration,
+                character illustration, 2d Game Design, Logo Design, Conceptual
+                Drawing, Background illustration,portrait illustration,
+                caricature illustration, t-shirt Design.
+                <h2 className="py-2">Hit me on "creativevalley9".</h2>
+              </p>
+              <h2 className="text-sm font-semibold">
+                Thanks,
+                <br />
+                creativevalley9
+              </h2>
             </div>
-            <div className="bg-gray-200 text-black text-center py-3 absolute bottom-0 w-full">
-              Know more
+            <div className="bg-[#1dbf73] text-white font-semibold text-center text-lg py-3 absolute bottom-0 w-full rounded-b-lg">
+              Visit my Fiver profile
             </div>
           </div>
-          <div className="w-full h-96 md:w-80 lg:w-72 xl:w-80 2xl:w-80 3xl:w-96 shadow-lg  rounded-lg hover:shadow-xl border border-slate-200 relative cursor-pointer">
-            <div className="flex justify-center text-center">
-              {/* <img src={image} className="h-full rounded-lg w-72 sm:w-80 md:w-96 3xl:w-80" /> */}
+          <div className="w-full h-[690px] md:w-80 lg:w-72 xl:w-96 2xl:w-[420px] 3xl:w-[480px] shadow-lg rounded-lg hover:shadow-xl border border-slate-200 relative cursor-pointer">
+            <div className="text-center">
+              {/* <img src={image} className="h-full rounded-lg w-72 sm:w-80 md:w-96 3xl:w-96" /> */}
+              <img
+                src={FiverrLogo}
+                alt=""
+                className="w-auto h-full rounded-xl"
+              />
               <img
                 src={FiverrImage}
-                className="rounded-t-lg object-cover w-full h-24"
+                className="rounded-t-lg object-cover w-full h-auto"
               />
             </div>
-            <div className="px-4 py-3 text-[15px]">
-              hi there, I'm prabir Sarkar. I've done 1100 plus projects with
-              850+ great reviews. I've completed 4 years degree course on
-              painting from the Indian college of arts and draftsmanship from
-              kolkata in India. From 2009 till now i'm working as a freelance
-              illustrator and painter.
+            <div className="px-4 py-3">
+              <h3 className="font-bold">About me</h3>
+              <h2 className="text-base font-medium py-2">
+                BEST QUALITY ON TIME AT FARE PRICE
+              </h2>
+              <p className="text-sm leading-[22px]">
+                hi there, I'm prabir Sarkar. I've done 1100 plus projects with
+                850+ great reviews. I've completed 4 years degree course on
+                painting from the Indian college of arts and draftsmanship from
+                kolkata in India. From 2009 till now i'm working as a freelance
+                illustrator and painter. I'm Working on children Book
+                illustration, African American children book illustration,
+                character illustration, 2d Game Design, Logo Design, Conceptual
+                Drawing, Background illustration,portrait illustration,
+                caricature illustration, t-shirt Design.
+                <h2 className="py-2">Hit me on "creativevalley9".</h2>
+              </p>
+              <h2 className="text-sm font-semibold">
+                Thanks,
+                <br />
+                creativevalley9
+              </h2>
             </div>
-            <div className="bg-gray-200 text-black text-center py-3 absolute bottom-0 w-full">
-              Know more
+            <div className="bg-[#1dbf73] text-white font-semibold text-center text-lg py-3 absolute bottom-0 w-full rounded-b-lg">
+              Visit my Fiver profile
             </div>
           </div>
-          <div className="w-full h-96 md:w-80 lg:w-72 xl:w-80 2xl:w-80 3xl:w-96 shadow-lg  rounded-lg hover:shadow-xl border border-slate-200 relative cursor-pointer">
-            <div className="flex justify-center text-center">
-              {/* <img src={image} className="h-full rounded-lg w-72 sm:w-80 md:w-96 3xl:w-80" /> */}
+          <div className="w-full h-[690px] md:w-80 lg:w-72 xl:w-96 2xl:w-[420px] 3xl:w-[480px] shadow-lg rounded-lg hover:shadow-xl border border-slate-200 relative cursor-pointer">
+            <div className="text-center">
+              {/* <img src={image} className="h-full rounded-lg w-72 sm:w-80 md:w-96 3xl:w-96" /> */}
+              <img
+                src={FiverrLogo}
+                alt=""
+                className="w-auto h-full rounded-xl"
+              />
               <img
                 src={FiverrImage}
-                className="rounded-t-lg object-cover w-full h-24"
+                className="rounded-t-lg object-cover w-full h-auto"
               />
             </div>
-            <div className="px-4 py-3 text-[15px]">
-              hi there, I'm prabir Sarkar. I've done 1100 plus projects with
-              850+ great reviews. I've completed 4 years degree course on
-              painting from the Indian college of arts and draftsmanship from
-              kolkata in India. From 2009 till now i'm working as a freelance
-              illustrator and painter.
+            <div className="px-4 py-3">
+              <h3 className="font-bold">About me</h3>
+              <h2 className="text-base font-medium py-2">
+                BEST QUALITY ON TIME AT FARE PRICE
+              </h2>
+              <p className="text-sm leading-[22px]">
+                hi there, I'm prabir Sarkar. I've done 1100 plus projects with
+                850+ great reviews. I've completed 4 years degree course on
+                painting from the Indian college of arts and draftsmanship from
+                kolkata in India. From 2009 till now i'm working as a freelance
+                illustrator and painter. I'm Working on children Book
+                illustration, African American children book illustration,
+                character illustration, 2d Game Design, Logo Design, Conceptual
+                Drawing, Background illustration,portrait illustration,
+                caricature illustration, t-shirt Design.
+                <h2 className="py-2">Hit me on "creativevalley9".</h2>
+              </p>
+              <h2 className="text-sm font-semibold">
+                Thanks,
+                <br />
+                creativevalley9
+              </h2>
             </div>
-            <div className="bg-gray-200 text-black text-center py-3 absolute bottom-0 w-full">
-              Know more
-            </div>
-          </div>
-          <div className="w-full h-96 md:w-80 lg:w-72 xl:w-80 2xl:w-80 3xl:w-96 shadow-lg  rounded-lg hover:shadow-xl border border-slate-200 relative cursor-pointer">
-            <div className="flex justify-center text-center">
-              {/* <img src={image} className="h-full rounded-lg w-72 sm:w-80 md:w-96 3xl:w-80" /> */}
-              <img
-                src={FiverrImage}
-                className="rounded-t-lg object-cover w-full h-24"
-              />
-            </div>
-            <div className="px-4 py-3 text-[15px]">
-              hi there, I'm prabir Sarkar. I've done 1100 plus projects with
-              850+ great reviews. I've completed 4 years degree course on
-              painting from the Indian college of arts and draftsmanship from
-              kolkata in India. From 2009 till now i'm working as a freelance
-              illustrator and painter.
-            </div>
-            <div className="bg-gray-200 text-black text-center py-3 absolute bottom-0 w-full">
-              Know more
+            <div className="bg-[#1dbf73] text-white font-semibold text-center text-lg py-3 absolute bottom-0 w-full rounded-b-lg">
+              Visit my Fiver profile
             </div>
           </div>
         </div>
