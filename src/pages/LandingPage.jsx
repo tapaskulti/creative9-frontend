a99ca4156ece2c6d5b09539d3baee129aa09593f
Tapas Chandra <tapaskulti@gmail.com>
2025-08-20 01:13:20 +0530

    banner image new size

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -6,8 +6,8 @@ import slide3 from "./../assets/slide3.jpg";
 import slide4 from "./../assets/slide4.jpg";
 import slide5 from "./../assets/slide5.jpg";
 import slide6 from "./../assets/slide6.jpg";
-import slide7 from "./../assets/slide7.jpg";
-import slide8 from "./../assets/slide8.jpg";
+// import slide7 from "./../assets/slide7.jpg";
+// import slide8 from "./../assets/slide8.jpg";
 
 // import {
 //   Facebook,
